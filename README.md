ddc4a59c5d6c577cc80b6aec4b46ca336dc83bed
Salam81 <14456384+Salam81@users.noreply.github.com>
2021-02-12 19:37:59 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # telegram-bot-forward-messages
 Telegram bot which will forward a message from a channel/chat to another chat.
+
+
+# Installation
+
+Requires [telethon](https://github.com/LonamiWebs/Telethon)
+
+```sh
+# Install telethon
+$ pip3 install -U telethon --user
+
+# Verify telethon's version:
+$ python3 -c "import telethon; print(telethon.__version__)"
+```
---
+++ script.py
@@ -0,0 +1,19 @@
+from telethon import TelegramClient, events, sync
+from os import environ
+
+# API Keys https://my.telegram.org/
+API_ID = environ['API_ID']
+API_HASH = environ['API_HASH']
+GROUP = environ['GROUP']
+RECIPIENT = environ['RECIPIENT'] # @username or me
+
+client = TelegramClient('bot', API_ID, API_HASH)
+
+@client.on(events.NewMessage(chats=GROUP))
+async def newMessageListener(event):
+    msg = event.message
+    print('Message: ' + msg.message)
+    await client.forward_messages(entity=RECIPIENT, messages=msg)
+
+with client:
+    client.run_until_disconnected()
